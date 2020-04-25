7f7970433e9655ac972c63f9eb80180913397972
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-04-25 15:40:50 -0700

    added card styles and displays data in numbers

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
-    try {
-        const response = await axios.get(url);
 
-        return response;
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        return { confirmed, recovered, deaths, lastUpdate };
+
     } catch (error) {
 
     }
