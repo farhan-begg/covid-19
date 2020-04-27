0d1106d1e26601f80d329d1774193b520198177a
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-04-27 13:16:15 -0700

    completed covid tracker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -6,24 +6,24 @@ import { fetchCountries } from '../../api';
 import styles from './CountryPicker.module.css';
 
 const Countries = ({ handleCountryChange }) => {
-    const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
-        const fetchAPI = async () => {
-            setCountries(await fetchCountries());
-        };
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
 
-        fetchAPI();
-    }, []);
+    fetchAPI();
+  }, []);
 
-    return (
-        <FormControl className={styles.formControl}>
-            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="">Global</option>
-                {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-            </NativeSelect>
-        </FormControl>
-    );
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">Global</option>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default Countries;
