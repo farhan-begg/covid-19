b4d4d1d1ffd903035a8da92a6c485112d36582e9
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-07 11:17:18 -0700

    Updates

---
+++ precache-manifest.8bd108212eefac1320e8dfa4bc90f553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82c20546ad92c0ac9889bfc312f98b3",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "1f4e7405790c10864eb0",
+    "url": "/covid-19/static/css/main.47cb0806.chunk.css"
+  },
+  {
+    "revision": "93a074ac0c445a14e138",
+    "url": "/covid-19/static/js/2.374aceaa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.374aceaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4e7405790c10864eb0",
+    "url": "/covid-19/static/js/main.8a907f8a.chunk.js"
+  },
+  {
+    "revision": "f6ecbbdf84e1f735035c",
+    "url": "/covid-19/static/js/runtime-main.1ae4e48e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
